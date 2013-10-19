5d7048d727e68d99075e28433de0eafb490efd72
Hova2 <nacho.mone@gmail.com>
2013-10-19 15:41:31 -0300

    Kit Kat

---
+++ Grupo 1 - Kit Kat/Site/listado.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <title>Viaje Ecológico</title>
+
+  
+  <link rel="stylesheet" href="css/foundation.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/responsive.css">
+  <link rel="stylesheet" href="css/font-awesome.css">
+  <link href='http://fonts.googleapis.com/css?family=GFS+Didot' rel='stylesheet' type='text/css'>
+  <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+  
+  <link rel="shortcut icon" href="ico/favicon.ico">
+  <link rel="apple-touch-icon" sizes="144x144" href="ico/apple-touch-icon-144.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="ico/apple-touch-icon-114.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="ico/apple-touch-icon-72.png">
+  <link rel="apple-touch-icon" href="ico/apple-touch-icon-57.png">
+  
+
+</head>
+<body style="border-top-color: #CEFF00;border-top-style: solid;border-top-width:5px;">
+<div id="main-container">
+
+<section id="services" class="pad-small">
+
+	<div class="row">
+		<div class="twelve columns" style="text-align: center;margin-bottom: 7%;">
+			<a href="index.html"><img src="img/ve_logo.svg" alt="Viaje Ecológico">
+			<h1 style="color: #719904;text-shadow: 1px 1px #B2DB05;">VIAJE ECOLÓGICO</h1></a>
+					<div id="copy-text" style="line-height: 15px;">Carpooling con conciencia ecológica, de verdad <div class="icon icon-leaf"></div>
+					</div>
+		</div>
+		</div>
+	
+
+<div class="row" style="height: 1%;display: block;"></div>	
+	
+	<div class="row service-row">
+		
+		<div class="large-10 large-centered columns " style="margin-top: 2%;">
+		
+						
+						
+						<div class="tablaviajes" >
+						
+						
+						<table>
+						<tr>
+						<th>Origen</th><th>Destino</th><th>Chofer</th><th>Fecha</th><th>Hora</th><th>Asientos disponibles</th><th>Mapas</th>
+						</tr>
+						<tr>
+						<td>Buenos Aires</td><td>Avellaneda</td><td>Matias Gonzalez</td><td>23/08/2013</td><td>19:00</td><td>3</td><td><a href="ruta1.html" target="_blank">Ver mapa</a></td>
+						</tr>
+						<tr>
+						<td>Chivilcoy</td><td>San Miguel</td><td>German Rodriguez</td><td>26/08/2013</td><td>05:00</td><td>1</td><td><a href="ruta2.html" target="_blank">Ver mapa</a></td>
+						</tr>
+						<tr>
+						<td>Merlo</td><td>Buenos Aires</td><td>Pedro Garcia</td><td>28/08/2013</td><td>15:00</td><td>5</td><td><a href="ruta3.html" target="_blank">Ver mapa</a></td>
+						</tr>
+						</table>
+						
+						</div>
+						            
+						            
+			
+					
+		</div>
+	</div>
+	
+	
+</section>
+
+
+<section id="contact">
+
+	
+
+	<div id="contact-content">
+	
+				
+		<div class="row social-row">
+			<div class="large-12 columnsB text-center">
+				
+				<div class="btn">
+					<h6>GOOGLE+</h6>
+				</div>
+				
+				<div class="btn">
+					<h6>TWITTER</h6>
+				</div>
+				
+				<div class="btn">
+					<h6>EMAIL</h6>
+				</div>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="large-12 columns text-center">
+				<div id="copy-text">Viaje Ecológico existe gracias a los servicios de Google <div class="icon icon-heart"></div></div>
+			</div>
+		</div>
+	
+	</div>
+
+	
+
+</section>
+
+
+
+
+</div><!-- end of Main Container-->
+
+  <script src="js/vendor/custom.modernizr.js"></script>
+  <script src="js/vendor/jquery.js"></script> 
+  <script src="js/foundation.min.js"></script>
+  <script src="js/scripts.js"></script>
+   <script src="js/smooth-scroll.js"></script>
+
+  
+  <script>
+    $(document).foundation();
+  </script>
+</body>
+</html>
