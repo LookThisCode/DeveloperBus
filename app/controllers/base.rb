a8ee35c0017b0848cb02d4ff49e9d549f0c5b8e1
Angel Botto <angelbotto@gmail.com>
2013-11-14 16:14:02 -0500

    metatags google+

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -4,7 +4,7 @@ Klou::App.controllers :base, :map => '/' do
 
   get :index do
     @title = "Klou - Lo dificil no parece facil, es facil!"
-    @descrip = "Lo dificil no parece facil, es facil!"
+    @descrip = "Somos una plataforma de aplicaciones soportada en Google Cloud Engine, la mejor forma de desarrollar e implementar aplicaciones web.  Buscamos agilidad, optimización de tiempo, experiencias únicas. Creemos que la tecnología debe ser fácil, debe ser rápida y entendible para todos."
     erb :"base/index"
   end
 
