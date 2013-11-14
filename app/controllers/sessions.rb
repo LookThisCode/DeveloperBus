8f1fc377550ac729f417da8dfb6c79cfa8173a36
Angel Botto <angelbotto@gmail.com>
2013-11-14 15:23:04 -0500

    Controlador de session para los usuarios

---
+++ app/controllers/sessions.rb
@@ -0,0 +1,27 @@
+Klou::App.controllers :sessions do
+  
+  get :login do
+
+  end
+
+  post :login do
+
+  end
+
+  get :register do
+
+  end
+
+  post :register do
+
+  end
+
+  get :forgot do
+
+  end
+
+  post :forgot do
+
+  end
+
+end
