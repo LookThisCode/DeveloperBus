09d1c5b1eece2b2f6f3cae7cab4f99fa008abcef
Laura Valentina Grajales Olarte <valegrajales@gmail.com>
2013-11-15 13:12:05 -0500

    Including group 7 code

---
+++ Season 2013/Bogota/Projects/7_hormigas/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BestPlaces::Application.config.secret_key_base = '7b849d058fb116f21cd14f0fa040c504d0202176d34562123772d74f71fee01f958e7d197dd6f531f6ccdbb31b948ca0a7cb6cbd7a5ecac43bd43ea09b884098'
